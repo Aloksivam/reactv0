ce470d80e2d633f424b9d53f0480660e538020bb
Alok <aloksivam@gmail.com>
2024-07-02 00:40:13 +0530

    completed switch theme project using context api

--- 10themetoggler/src/App.jsx
+++ 10themetoggler/src/App.jsx
@@ -1,16 +1,30 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Card from './components/Card'
 import ThemeBtn from './components/ThemeBtn'
-
+import { ThemeProvider } from './contexts/theme'
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState('light')
+  const darkTheme=()=>{setThemeMode('dark')}
+  const lightTheme=()=>{setThemeMode('light')}
+  useEffect(()=>{document.querySelector('html').classList.remove('dark',"light")
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
 
   return (
-<>
-<Card/>
-<ThemeBtn/>
-</>
+<ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
+<div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+</ThemeProvider>
   )
 }
 
--- 10themetoggler/src/components/ThemeBtn.jsx
+++ 10themetoggler/src/components/ThemeBtn.jsx
@@ -1,8 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import useTheme from '../contexts/theme';
+
+
 
 export default function ThemeBtn() {
-    const onChangeBtn=()=>{}
+
+    const {themeMode,lightTheme,darkTheme} = useTheme()
+    const onChangeBtn=(e)=>{
+        const darkModeStatus = e.currentTarget.checked
+        if(darkModeStatus){
+            darkTheme()
+        }
+        else{
+            lightTheme()
+        }
+    }
     return (
         <label className="relative inline-flex items-center cursor-pointer">
             <input
